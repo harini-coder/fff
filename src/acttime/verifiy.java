5b2597c2d17786ab8ab922f54d4ef9b4478c8980
harini-coder <59640329+harini-coder@users.noreply.github.com>
2020-01-11 13:17:27 +0530

    Revert "changes for i and if loop"

--- src/acttime/verifiy.java
+++ src/acttime/verifiy.java
@@ -4,17 +4,9 @@ public class verifiy {
 	
 
 	public static void main(String[] args) {
-		for(int i=0; i<=10; i++)
-		{ if(i% 2==0){
-			System.out.print(i);
 		
-		}
-		}
-			
-
-		}
 		
 
 	}
 
-
+}
